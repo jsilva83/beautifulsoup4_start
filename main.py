e19191e9bf5e12602a0c2c01b9812dfed959ac1e
jsilva83 <jorge.h.silva@gmail.com>
2022-01-28 07:15:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Import external modules.
+from bs4 import BeautifulSoup
+
+
+def run_main():
+    with open('website.html', 'r') as a_html_file:
+        a_file_contents = a_html_file.read()
+        pass
+    return
+
+
+if __name__ == '__main__':
+    run_main()
